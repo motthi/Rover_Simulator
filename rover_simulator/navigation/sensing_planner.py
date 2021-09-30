0f145cd4c359de42a9f5d95f3ae1c95915e0ec9e
Pasises <45708319+pasises@users.noreply.github.com>
2021-09-30 14:13:22 +0900

    feat : update sensing planner

--- rover_simulator/navigation/sensing_planner.py
+++ rover_simulator/navigation/sensing_planner.py
@@ -1,6 +1,6 @@
 import numpy as np
 from rover_simulator.core import SensingPlanner
-from rover_simulator.utils import angle_to_range
+from rover_simulator.utils import angle_to_range, isInRange
 
 
 class SimpleSensingPlanner(SensingPlanner):
@@ -10,26 +10,29 @@ class SimpleSensingPlanner(SensingPlanner):
         sense_interval_distance: float = 3.0,
         sense_interval_angle: float = 3 * np.pi / 16
     ) -> None:
-        self.sense_interval_ditance = sense_interval_distance
+        self.sense_interval_distance = sense_interval_distance
         self.sense_interval_angle = sense_interval_angle
         self.rover_pose = rover_pose
         self.previous_pose = rover_pose
-        self.distance = self.sense_interval_ditance
+        self.previous_sensing_pose = np.array([-float('inf'), -float('inf')])
+        self.distance = self.sense_interval_distance
 
     def decide(self, rover_pose: np.ndarray) -> None:
-        self.distance += np.linalg.norm(self.previous_pose[0:2] - rover_pose[0:2])
-        self.previous_pose = rover_pose
-        d_theta = angle_to_range(np.arctan2(rover_pose[1] - self.rover_pose[1], rover_pose[0] - self.rover_pose[0]) - self.rover_pose[2])
-        if np.abs(d_theta) > self.sense_interval_angle:
-            self.rover_pose = rover_pose
+        dist = np.linalg.norm(self.previous_sensing_pose[0:2] - rover_pose[0:2])
+        if dist > self.sense_interval_distance:
+            self.previous_sensing_pose = rover_pose
             return True
-        d_theta = angle_to_range(self.rover_pose[2] - rover_pose[2])
-        if np.abs(d_theta) > self.sense_interval_angle:
-            self.rover_pose = rover_pose
+        d_theta = angle_to_range(
+            np.arctan2(
+                rover_pose[1] - self.previous_sensing_pose[1], rover_pose[0] - self.previous_sensing_pose[0]
+            ) - self.previous_sensing_pose[2]
+        )
+        if dist > 1e-5 and not isInRange(d_theta, -self.sense_interval_angle, self.sense_interval_angle):
+            # dist > 1e-5 => Not Moved
+            self.previous_sensing_pose = rover_pose
             return True
-
-        if self.distance >= self.sense_interval_ditance:
-            self.rover_pose = rover_pose
-            self.distance = 0.0
+        d_theta = rover_pose[2] - self.previous_sensing_pose[2]
+        if np.abs(d_theta) > self.sense_interval_angle:
+            self.previous_sensing_pose = rover_pose
             return True
         return False
