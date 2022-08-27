ba96567d779a600c6fbcbbeb72c02ee805808c19
motthi <45708319+pasises@users.noreply.github.com>
2022-08-28 00:26:21 +0900

    feat : fix cc-rrt

--- rover_simulator/navigation/path_planner/sampling_base.py
+++ rover_simulator/navigation/path_planner/sampling_base.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 import random
 import math
-import time
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 from scipy.spatial import cKDTree
-from rover_simulator.navigation.localizer import KalmanFilter
 from rover_simulator.navigation.path_planner import PathPlanner
-from rover_simulator.utils import angle_to_range, sigma_ellipse, GeoEllipse, cov_to_ellipse, ellipse_collision
+from rover_simulator.utils.utils import angle_to_range, GeoEllipse, cov_to_ellipse, ellipse_collision
+from rover_simulator.utils.motion import covariance_transition, state_transition
+from rover_simulator.utils.draw import set_fig_params, draw_obstacles, draw_start, draw_goal, sigma_ellipse
 
 
 class RRT(PathPlanner):
@@ -53,7 +52,6 @@ class RRT(PathPlanner):
         self.obstacle_list = [[obstacle.pos[0], obstacle.pos[1], obstacle.r + enlarge_range] for obstacle in known_obstacles]
         # obstacle_positions = [obstacle.pos for obstacle in known_obstacles] if not known_obstacles is None else None
         # self.obstacle_kdTree = cKDTree(obstacle_positions)
-
         self.name = "RRT"
 
     def dist_nodes(self, n1: Node, n2: Node) -> float:
@@ -161,16 +159,6 @@ class RRT(PathPlanner):
         waypoints.reverse()
         return waypoints
 
-    def set_fig_params(self, figsize, xlim, ylim):
-        self.fig = plt.figure(figsize=figsize)
-        ax = self.fig.add_subplot(111)
-        ax.set_aspect('equal')
-        ax.set_xlim(xlim[0], xlim[1])
-        ax.set_ylim(ylim[0], ylim[1])
-        ax.set_xlabel("X [m]", fontsize=10)
-        ax.set_ylabel("Y [m]", fontsize=10)
-        return ax
-
     def draw_path(self, ax):
         if self.planned_path is None:
             return
@@ -183,14 +171,6 @@ class RRT(PathPlanner):
             if node.parent:
                 ax.plot(node.path_x, node.path_y, c="cyan")
 
-    def draw_obstacles(self, ax, obstacles, enlarge_range):
-        for obstacle in obstacles:
-            enl_obs = patches.Circle(xy=(obstacle.pos[0], obstacle.pos[1]), radius=obstacle.r + enlarge_range, fc='black', ec='black', zorder=-1.0)
-            ax.add_patch(enl_obs)
-        for obstacle in obstacles:
-            obs = patches.Circle(xy=(obstacle.pos[0], obstacle.pos[1]), radius=obstacle.r, fc='black', ec='black', zorder=-1.0)
-            ax.add_patch(obs)
-
     def draw(
             self,
             xlim: list[float], ylim: list[float],
@@ -198,8 +178,8 @@ class RRT(PathPlanner):
             obstacles: list = [],
             enlarge_range: float = 0.0,
     ):
-        ax = self.set_fig_params(figsize, xlim, ylim)
-        self.draw_obstacles(ax, obstacles, enlarge_range)
+        fig, ax = set_fig_params(figsize, xlim, ylim)
+        draw_obstacles(ax, obstacles, enlarge_range)
         self.draw_nodes(ax)
         self.draw_path(ax)
         ax.plot(self.start_node.x, self.start_node.y, "xr")
@@ -413,41 +393,35 @@ class ChanceConstrainedRRT(RRT):
     class Node(RRT.Node):
         def __init__(self, x: float, y: float, head: float = 0.0, cov: np.ndarray = np.diag([1e-10, 1e-10, 1e-10])) -> None:
             super().__init__(x, y)
-            self.childs = []
             self.head = head
             self.cov = cov
             self.cost_lb = None         # Lower bound cost
             self.cost_ub = float('inf')  # Upper bound cost
             self.cost_fs = 0.0          # Cost from start
-
-        def set_lower_bound_cost(self, goal_pos) -> None:
-            self.cost_lb = math.hypot(self.x - goal_pos[0], self.y - goal_pos[1])
-
-        def set_upper_bound_cost(self) -> None:
-            self.cost_ub = 0.0
+            self.childs = []
 
     def __init__(
         self,
-        start_pos: np.ndarray = None, goal_pos: np.ndarray = None, start_cov: np.ndarray = None, start_head: float = None,
-        explore_region: list = [[0, 20], [0, 20]], known_obstacles: list = [], enlarge_range: float = 0, expand_distance: float = 3.0, cost_func: function = None,
-        goal_sample_rate: float = 0.3, path_resolution: float = 0.5,
-        num_nearest_node: int = 6, p_safe: float = 0.99, motion_noise_stds: dict = {"nn": 0.1, "no": 0.0001, "on": 0.013, "oo": 0.02},
-        k: float = 1.0
+        start_pos: np.ndarray = None, goal_pos: np.ndarray = None, start_cov: np.ndarray = None,
+        start_head: float = None, motion_noise_stds: dict = {"nn": 0.1, "no": 0.0001, "on": 0.013, "oo": 0.02},
+        explore_region: list = [[0, 20], [0, 20]], known_obstacles: list = [], enlarge_range: float = 0, expand_distance: float = 3.0,
+        cost_func: function = None, steer_func: function = None,
+        goal_sample_rate: float = 0.3, goal_region=2.0,
+        num_nearest_node: int = 6, p_safe: float = 0.99, k: float = 1.0
     ) -> None:
-        super().__init__(start_pos, goal_pos, explore_region, known_obstacles, enlarge_range, expand_distance, goal_sample_rate, path_resolution, cost_func)
+        super().__init__(start_pos, goal_pos, explore_region, known_obstacles, enlarge_range, expand_distance, goal_sample_rate, 0, cost_func)
         if start_pos is not None and goal_pos is not None and start_cov is not None:
             if start_head is None:
                 start_head = np.arctan2(goal_pos[1] - start_pos[1], goal_pos[0] - start_pos[0])
             self.start_node = self.Node(start_pos[0], start_pos[1], start_head, start_cov)
             self.goal_node = self.Node(goal_pos[0], goal_pos[1], None, None)
-        else:
-            self.start_node = None
-            self.goal_node = None
         self.num_nearest_node = num_nearest_node
         self.p_safe = p_safe
         self.motion_noise_stds = motion_noise_stds
         self.sampled_pts = []
+        self.control_inputs = self.select_inputs if steer_func is None else steer_func
         self.k = k
+        self.goal_region = goal_region
         self.name = "CC-RRT"
 
     def calculate_path(self, max_time: float = 5.0, max_iter: int = 200, *kargs) -> list:
@@ -457,11 +431,9 @@ class ChanceConstrainedRRT(RRT):
             raise ValueError("goal_node is None")
 
         self.node_list = [self.start_node]
-        self.sampled_pts = []
         self.first_sample = True
-        s_time = time.time()
-        while time.time() - s_time < max_time:
-            self.expand_tree(max_iter)
+        for _ in range(max_iter):
+            self.expand_tree()
 
         self.planned_path = self.generate_final_course()
         return [np.array([n.x, n.y]) for n in self.planned_path]
@@ -469,17 +441,13 @@ class ChanceConstrainedRRT(RRT):
     def generate_final_course(self):
         node_poses = [np.array([n.x, n.y]) for n in self.node_list]
         nodes_kdtree = cKDTree(node_poses)
-        idxes = nodes_kdtree.query_ball_point(np.array([self.goal_node.x, self.goal_node.y]), r=0.5 * self.expand_dis)
-        # print(idxes)
+        idxes = nodes_kdtree.query_ball_point(np.array([self.goal_node.x, self.goal_node.y]), r=self.goal_region)
         min_cost = float('inf')
         min_idx = None
         for idx in idxes:
             if min_cost > self.node_list[idx].cost_fs:
                 min_cost = self.node_list[idx].cost_fs
                 min_idx = idx
-            # print(self.calc_dist_to_goal(self.node_list[idx].x, self.node_list[idx].y))
-            # print(self.node_list[idx].cost_fs)
-        # print(min_idx, min_cost)
         if min_idx is None:
             planned_path = []
         else:
@@ -491,67 +459,52 @@ class ChanceConstrainedRRT(RRT):
             planned_path.reverse()
         return planned_path
 
-    def expand_tree(self, max_iter: int) -> None:
-        for _ in range(max_iter):
-            trg_node = self.sample_new_node()
-            near_idxes = self.get_m_nearest_nodes_indexes(self.node_list, trg_node)
-            # pruned_idxes = []
-            node_list = copy.copy(self.node_list)
-            for idx in near_idxes:
-                # if idx in pruned_idxes:
-                #     continue
-                near_node = node_list[idx]
-                new_nodes, _ = self.connect_to_target(near_node, trg_node)
-                for new_node in new_nodes:
-                    self.node_list.append(new_node)
-                    nodes_to_goal, reached_goal = self.connect_to_target(new_node, self.goal_node)
-                    for n in nodes_to_goal:
-                        self.node_list.append(n)
-                    if reached_goal is True:
-                        ub_cost = 0.0
-                        nodes_to_goal.reverse()
-                        node = nodes_to_goal[-1]
-                        while not node.parent is None:
-                            p_node = node.parent
-                            ub_cost += self.cost(p_node, node)
-                            # if ub_cost > node.cost_ub:
-                            #     # self.node_list.remove(new_node)
-                            #     # for child in new_node.childs:
-                            #     #     try:
-                            #     #         self.node_list.remove(child)
-                            #     #     except:
-                            #     #         pass
-                            #     # self.node_list.pop(-1)  # Must prune portions of the tree
-                            #     break
-                            if node.cost_ub > ub_cost:
-                                node.cost_ub = ub_cost
-                            node.cost_fs = p_node.cost_fs + self.cost(node, p_node)
-                            # for c_node in node.childs:
-                            #     if c_node.cost_lb + self.cost(c_node, node) > node.cost_ub:
-                            #         pruned_idxes += self.prune_childs(c_node, node_list)
-                            node = p_node
+    def expand_tree(self) -> None:
+        trg_node = self.sample_new_node()
+        near_idxes = self.get_m_nearest_nodes_indexes(self.node_list, trg_node)
+        pruned_idxes = []
+        node_list = copy.copy(self.node_list)
+        for idx in near_idxes:
+            if idx in pruned_idxes:
+                continue
+            near_node = node_list[idx]
+            new_nodes, _ = self.connect_to_target(near_node, trg_node)
+            for new_node in new_nodes:
+                new_node.cost_fs = new_node.parent.cost_fs + self.cost(new_node.parent, new_node)
+                self.node_list.append(new_node)
+                nodes_to_goal, reached_goal = self.connect_to_target(new_node, self.goal_node)
+                if reached_goal is True:
+                    ub_cost = 0.0
+                    node = nodes_to_goal[-1]
+                    while not node.parent is None:
+                        p_node = node.parent
+                        ub_cost += self.cost(p_node, node)
+                        lb_cost = self.cost(self.goal_node, node)
+                        if ub_cost < lb_cost:
+                            pruned_idxes += self.prune_childs(node, node_list)
+                        node = p_node
 
     def prune_childs(self, node: Node, node_list: list) -> None:
         pruned_idxes = []
         for child in node.childs:
-            try:
+            if child in self.node_list:
                 idx = node_list.index(child)
                 self.node_list.remove(child)
+                child.parent.childs.remove(child)
                 pruned_idxes.append(idx)
                 pruned_idxes += self.prune_childs(child, node_list)
-            except:
-                pass
         return pruned_idxes
 
     def sample_new_node(self) -> Node:
-        if random.random() > self.goal_sample_rate and self.first_sample is False:
-            rnd = self.Node(
-                random.uniform(self.explore_x_min, self.explore_x_max),
-                random.uniform(self.explore_y_min, self.explore_y_max)
-            )
-        else:  # goal point sampling
-            rnd = self.Node(self.goal_node.x, self.goal_node.y)
-        self.first_sample = False
+        # if random.random() > self.goal_sample_rate and self.first_sample is False:
+        rnd = self.Node(
+            random.uniform(self.explore_x_min, self.explore_x_max),
+            random.uniform(self.explore_y_min, self.explore_y_max)
+        )
+        # else:  # goal point sampling
+        #     rnd = self.Node(self.goal_node.x, self.goal_node.y)
+        # rnd = self.Node(self.goal_node.x, self.goal_node.y)
+        # self.first_sample = False
         self.sampled_pts.append(rnd)
         return rnd
 
@@ -562,54 +515,58 @@ class ChanceConstrainedRRT(RRT):
         dist_to_trg = float('inf')
         prev_node = node
         while True:
-            control_inputs = self.select_inputs(node, trg_node)
-            new_node = self.simulate_one_step(node, control_inputs)
-            x_tk = np.array([new_node.x, new_node.y])
-            p_tk = new_node.cov
-            dist_to_trg = math.hypot(new_node.x - trg_node.x, new_node.y - trg_node.y)
+            control_inputs = self.control_inputs(np.array([node.x, node.y, node.head]), np.array([trg_node.x, trg_node.y, trg_node.head]))
+            x_tk, p_tk = self.steer(node, control_inputs)
+            dist_to_trg = math.hypot(x_tk[0] - trg_node.x, x_tk[1] - trg_node.y)
+            new_node = self.Node(x_tk[0], x_tk[1], x_tk[2], p_tk)
             if not self.is_safe(x_tk, p_tk):
                 break
             if dist_to_trg <= 0.5 * self.expand_dis:
-                new_nodes.append(new_node)
-                new_node.parent = prev_node
-                new_node.cost_fs = prev_node.cost_fs + self.cost(new_node, prev_node)
-                prev_node.childs.append(new_node)
+                # new_node.cost_fs = prev_node.cost_fs + self.cost(new_node, prev_node)
+                self.connect_nodes(prev_node, new_node)
                 new_node.path_x = [prev_node.x, new_node.x]
                 new_node.path_y = [prev_node.y, new_node.y]
-                prev_node = new_node
+                new_node.cost_lb = self.cost(new_node, self.goal_node)
+                new_nodes.append(new_node)
                 break
             if math.hypot(new_node.x - prev_node.x, new_node.y - prev_node.y) > 1.0:
-                new_nodes.append(new_node)
-                new_node.parent = prev_node
-                new_node.cost_fs = prev_node.cost_fs + self.cost(new_node, prev_node)
-                prev_node.childs.append(new_node)
+                # new_node.cost_fs = prev_node.cost_fs + self.cost(new_node, prev_node)
+                self.connect_nodes(prev_node, new_node)
                 new_node.path_x = [prev_node.x, new_node.x]
                 new_node.path_y = [prev_node.y, new_node.y]
+                new_node.cost_lb = self.cost(new_node, self.goal_node)
+                new_nodes.append(new_node)
                 prev_node = new_node
             node = new_node
         return new_nodes, (dist_to_trg < self.expand_dis and len(new_nodes) > 0)
+
+    def connect_nodes(self, pn: Node, cn: Node) -> None:
+        cn.parent = pn
+        pn.childs.append(cn)
 
     def get_m_nearest_nodes_indexes(self, node_list: list[Node], rnd_node: Node) -> list:
         dist_costs = [self.dist_nodes(node, rnd_node) for node in node_list]    # rnd_nodeから各nodeへの距離
         fs_costs = [node.cost_fs for node in node_list]  # スタート地点から各nodeまでの距離
 
         # コスト重視
+        # print(dist_costs)
+        # print(fs_costs)
         costs = [2.0 * dist + self.k * fs for (dist, fs) in zip(dist_costs, fs_costs)]
         costs_sorted = sorted(costs)
         mininds = []
         for i, d in enumerate(costs_sorted):
-            if i >= self.num_nearest_node / 2:
+            if i >= self.num_nearest_node:
                 break
             mininds.append(costs.index(d))
 
         # コストと距離の中間
-        costs = [1.5 * dist + self.k * fs for (dist, fs) in zip(dist_costs, fs_costs)]
-        costs_sorted = sorted(costs)
-        mininds = []
-        for i, d in enumerate(costs_sorted):
-            if i >= self.num_nearest_node / 2:
-                break
-            mininds.append(costs.index(d))
+        # costs = [1.5 * dist + self.k * fs for (dist, fs) in zip(dist_costs, fs_costs)]
+        # costs_sorted = sorted(costs)
+        # mininds = []
+        # for i, d in enumerate(costs_sorted):
+        #     if i >= self.num_nearest_node / 2:
+        #         break
+        #     mininds.append(costs.index(d))
 
         # 距離重視
         # costs = [dist + 0.0 * fs for (dist, fs) in zip(dist_costs, fs_costs)]
@@ -621,25 +578,24 @@ class ChanceConstrainedRRT(RRT):
             # mininds.append(costs.index(d))
         return mininds
 
-    def select_inputs(self, node: Node, trg_node: Node) -> list:
+    def select_inputs(self, cur_pose, trg_pose) -> list:
         L = 1.0
         v = 1.0
-        theta = np.arctan2(trg_node.y - node.y, trg_node.x - node.x) - node.head
+        theta = np.arctan2(trg_pose[1] - cur_pose[1], trg_pose[0] - cur_pose[0]) - cur_pose[2]
         theta = angle_to_range(theta)
         w = 2 * v * np.sin(theta) / L
         return v, w
 
-    def simulate_one_step(self, from_node: Node, control_inputs: list[float, float]) -> Node:
+    def steer(self, from_node: Node, control_inputs: list[float, float]) -> Node:
+        dt = 0.5
         prev_pose = np.array([from_node.x, from_node.y, from_node.head])
-        kf = KalmanFilter(prev_pose, from_node.cov, self.motion_noise_stds)
-        new_pose = kf.estimate_pose(prev_pose, control_inputs, 0.5)
-        new_node = self.Node(new_pose[0], new_pose[1], new_pose[2], kf.belief.cov)
-        new_node.parent = from_node
-        new_node.path_x = [from_node.x, new_node.x]
-        new_node.path_y = [from_node.y, new_node.y]
-        new_node.cost_lb = self.cost(new_node, self.goal_node)
-        new_node.cost_fs = from_node.cost_fs + self.cost(new_node, from_node)
-        return new_node
+        nu, omega = control_inputs
+        if abs(omega) < 1e-5:
+            omega = 1e-5  # 値が0になるとゼロ割りになって計算ができないのでわずかに値を持たせる
+        control_inputs = [nu, omega]
+        new_pose = state_transition(prev_pose, control_inputs, dt)
+        new_cov = covariance_transition(prev_pose, from_node.cov, self.motion_noise_stds, control_inputs, dt)
+        return new_pose, new_cov
 
     def is_safe(self, x: np.ndarray, cov: np.ndarray) -> bool:
         if x[0] < 0 or x[0] > 20 or x[1] < 0 or x[1] > 20:
@@ -660,19 +616,19 @@ class ChanceConstrainedRRT(RRT):
             draw_ellipse: bool = True,
             draw_result_only: bool = False
     ) -> None:
-        ax = self.set_fig_params(figsize, xlim, ylim)
-        self.draw_obstacles(ax, obstacles, enlarge_range)
+        self.fig, ax = set_fig_params(figsize, xlim, ylim)
+        draw_obstacles(ax, obstacles, enlarge_range)
 
         # Draw all nodes
         if draw_result_only is False:
-            self.draw_node_childs(ax, self.start_node, draw_ellipse)
-            # for n in self.node_list:
-            #     if n.parent:
-            #         ax.plot(n.path_x, n.path_y, color="cyan")
-            #         if draw_ellipse is True:
-            #             p = np.array([n.x, n.y, n.head])
-            #             e = sigma_ellipse(p[0:2], n.cov[0:2, 0:2], 3, "blue")
-            #             ax.add_patch(e)
+            # self.draw_node_childs(ax, self.start_node, draw_ellipse)
+            for n in self.node_list:
+                if n.parent:
+                    ax.plot(n.path_x, n.path_y, color="cyan")
+                    if draw_ellipse is True:
+                        p = np.array([n.x, n.y, n.head])
+                        e = sigma_ellipse(p[0:2], n.cov[0:2, 0:2], 3, "blue")
+                        ax.add_patch(e)
 
         # Draw path from start to goal
         for n in self.planned_path:
